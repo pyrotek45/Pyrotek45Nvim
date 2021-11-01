f5f7f084a2415f6c31390be21367b56accc111b8
pyrotek45 <51059135+pyrotek45@users.noreply.github.com>
2021-11-01 18:41:36 -0400

    fixed bug copyposta error

--- Pyrotek45Nvim_install.sh
+++ Pyrotek45Nvim_install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 post_install() {
-    echo ----------------------------------------------------
-    echo Neovim will install the plugins on its first launch
-    echo Just reload neovim to use as normal
-    echo Setting up neovim and vim-plug
-    echo ----------------------------------------------------
+    echo "----------------------------------------------------"
+    echo "Neovim will install the plugins on its first launch"
+    echo "Just reload neovim to use as normal"
+    echo "Setting up neovim and vim-plug"
+    echo "----------------------------------------------------"
 }
 
 install_normal() {
     mkdir -p ~/.config/nvim
     curl https://raw.githubusercontent.com/pyrotek45/Pyrotek45Nvim/main/init.vim -o ~/.config/nvim/init.vim
 }
-
 install_flatpak() {
     mkdir -p ~/.var/app/io.neovim.nvim/config/nvim
     curl https://raw.githubusercontent.com/pyrotek45/Pyrotek45Nvim/main/init.vim -o ~/.var/app/io.neovim.nvim/config/nvim/init.vim
 }
 
+
 if [ "$1" = "" ]
 then
-    echo Welcome to the Pyrotek45Nvim installer!
-    echo You can use the options as commandline arguments to this script
-    echo 1. Install to normal installation
-    echo 2. Install to flatpak installation
-    echo 3. Install to both normal and flatpak installations
-    echo 4. Quit
+    echo "Welcome to the Pyrotek45Nvim installer!"
+    echo "You can use the options as commandline arguments to this script"
+    echo "1. Install to normal installation"
+    echo "2. Install to flatpak installation"
+    echo "3. Install to both normal and flatpak installations"
+    echo "4. Quit"
 
-    printf "(1,2,3,4) "
+    printf "(1,2,3,4)"
     read option
 else
     option="$1"
@@ -51,6 +51,7 @@ case $option in
         exit
         ;;
     *)
-        echo Please choose between the numbers 1, 2, 3 and 4 or pass them as arguments after the script
+        echo "Please choose between the numbers 1, 2, 3 and 4 or pass them as arguments after the script"
         exit 1
-esa
+        ;;
+esac
